6a5499b66f3e5d3594b28de6aad8cceef2e14de0
hazel-carbajal <66264219+hazel-carbajal@users.noreply.github.com>
2021-05-09 22:34:50 -0400

    renamed displayForecast

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function formatDateTime(timestamp) {
   return `${dayOfWeek} ${month}/${day} ${hour}:${minutes}`;
 }
 
-function displayForecast() {
+function displayShortTermForecast(response) {
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -59,7 +59,7 @@ function displayForecast() {
 function getForecast(coordinates) {
   let apiKey = "906eb52d0f658b640312a572c13864cf";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&&appid=${apiKey}&unit=metric`;
-  axios.get(apiUrl).then(displayForecast);
+  axios.get(apiUrl).then(displayShortTermForecast);
 }
 
 function displayWeatherForSearchedCity(response) {
